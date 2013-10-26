37d517c3130e81860d2a17ad18b63b427a521985
Clayton Stangeland <clayton.stangeland@gmail.com>
2013-10-26 15:24:22 -0500

    more templates

--- Actors/Extensions/EqualsStructs.g.cs
+++ Actors/Extensions/EqualsStructs.g.cs
@@ -1,1 +1,70 @@
-﻿ErrorGeneratingOutput
+﻿using System;
+namespace Actors
+{
+    partial struct ActorId : IEquatable<ActorId>
+    {
+        public override bool Equals(object other)
+        {		
+			if(other == null || other.GetType() != GetType())
+				return false;
+            return Equals((ActorId)other);
+        }		
+
+         public static bool operator==(ActorId left, ActorId right)
+		 {
+            if (ReferenceEquals(left, right))
+			     return true;
+			else if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+			      return false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator!=(ActorId left, ActorId right)
+        {
+            if (ReferenceEquals(left, right))
+                return false;
+            else if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+                return true;
+            else
+                return !left.Equals(right);
+        }
+    }
+}
+
+namespace Actors.Network
+{
+    partial struct NodeId : IEquatable<NodeId>
+    {
+        public override bool Equals(object other)
+        {		
+			if(other == null || other.GetType() != GetType())
+				return false;
+            return Equals((NodeId)other);
+        }		
+
+         public static bool operator==(NodeId left, NodeId right)
+		 {
+            if (ReferenceEquals(left, right))
+			     return true;
+			else if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+			      return false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator!=(NodeId left, NodeId right)
+        {
+            if (ReferenceEquals(left, right))
+                return false;
+            else if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+                return true;
+            else
+                return !left.Equals(right);
+        }
+    }
+}
+
+
+
+
